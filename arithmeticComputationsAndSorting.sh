d4f8faaac789e049802a98a4eaef387317a03935
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2020-12-29 14:04:13 +0530

    ADD.added a file which reads dictionary values into array

--- arithmeticComputationsAndSorting.sh
+++ arithmeticComputationsAndSorting.sh
@@ -7,5 +7,10 @@ arithmeticOperation[ "a+b*c" ]=$(( a+b*c ))
 arithmeticOperation[ "a*b+c" ]=$(( a*b+c ))
 arithmeticOperation[ "c+a/b" ]=$(( c+a/b ))
 arithmeticOperation[ "a%b+c" ]=$(( a%b+c ))
+array[0]=$(( arithmeticOperation[ "a+b*c" ] ))
+array[1]=$(( arithmeticOperation[ "a*b+c" ] ))
+array[2]=$(( arithmeticOperation[ "c+a/b" ] ))
+array[3]=$(( arithmeticOperation[ "a%b+c" ] ))
 echo ${!arithmeticOperation[@]}
 echo ${arithmeticOperation[@]}
+echo ${array[@]}
