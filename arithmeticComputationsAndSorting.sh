a954b9e1b653941dfcdbca45235027257bbee9a5
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2020-12-29 13:53:22 +0530

    ADD.added a file which stores all computations results in dictionary

--- arithmeticComputationsAndSorting.sh
+++ arithmeticComputationsAndSorting.sh
@@ -2,4 +2,10 @@
 read -p "enter a value in a " a
 read -p "enter a value in b " b
 read -p "enter a value in c " c
-arithmeticOperation=$(( a%b+c ))
+declare -A arithmeticOperation
+arithmeticOperation[ "a+b*c" ]=$(( a+b*c ))
+arithmeticOperation[ "a*b+c" ]=$(( a*b+c ))
+arithmeticOperation[ "c+a/b" ]=$(( c+a/b ))
+arithmeticOperation[ "a%b+c" ]=$(( a%b+c ))
+echo ${!arithmeticOperation[@]}
+echo ${arithmeticOperation[@]}
